0b4e3bbb7c92302af47fcf314938f1102b5ef114
Chris <yuxiaohui0626@gmail.com>
2024-09-26 09:37:22 +0800

    fix(fieldUtil/checkEmail)

--- src/util/fieldUtil.ts
+++ src/util/fieldUtil.ts
@@ -19,7 +19,7 @@ export function checkEmail(email: string) {
   if (checkFieldIsEmpty(email, 'email')) {
     return false
   }
-  const reg = /^([A-Za-z0-9_\-.\u4e00-\u9fa5])+@student.xjtlu.edu.cn$/;
+  const reg = /^([A-Za-z0-9_\-.\u4e00-\u9fa5])+@xjtlu.edu.cn$/;
   if (!reg.test(email)) {
     uni.showToast({
       title: 'Please input your XJTLU email',
