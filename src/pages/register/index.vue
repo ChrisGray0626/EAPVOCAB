9fdb7873037f838ed6f20b979853cd55c0d58983
ChrisGray <yuxiaohui0626@gmail.com>
2024-10-17 10:48:15 +0800

    feat(fieldUtil): Remove email reg check
    feat(register): Email check

--- src/pages/register/index.vue
+++ src/pages/register/index.vue
@@ -143,10 +143,10 @@ export default defineComponent({
       if (checkFieldIsEmpty(this.module, 'module')) {
         return;
       }
-      // TODO Check email
-      // if (!checkEmail(this.email)) {
-      //   return;
-      // }
+      // Check email
+      if (!checkEmail(this.email)) {
+        return;
+      }
       // Check captcha
       if (checkFieldIsEmpty(this.captcha, 'captcha')) {
         return;
